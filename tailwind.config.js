1f54c7b6760377cc89ee57fa4f5f9ce079b48b6a
Katsun1246 <96658284+Katsun1236@users.noreply.github.com>
2026-04-21 19:25:01 +0000

    fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 content: [
-    "./*.html",         // Tous les HTML à la racine
-    "./**/*.html",      // Tous les HTML dans TOUS les sous-dossiers (blog, admin, etc.)
-    "./js/**/*.js",     // Tes scripts JS
-    "./data/*.json"     // Important car tes articles de blog sont là
+    "index.html",
+    "services.html",
+    "realisations.html",
+    "contact.html",
+    "stats.html",
+    "./*.html",
+    "./blog/**/*.html",
+    "./js/**/*.js",
+    "./data/**/*.json"
   ],
   theme: {
     extend: {
